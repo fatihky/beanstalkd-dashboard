ee0d6700f77afdc3898082b88f869ccc908b36b9
Fatih Kaya <1994274@gmail.com>
2025-08-17 00:32:12 +0300

    display an hourglass for tube actions

--- src/components/tubes/tube-actions.tsx
+++ src/components/tubes/tube-actions.tsx
@@ -1,41 +1,48 @@
 import { useMutation } from '@tanstack/react-query';
-import { Pause, Play } from 'lucide-react';
+import { Hourglass, Pause, Play } from 'lucide-react';
 import { useTRPC } from '@/utils/trpc';
 import { Button } from '../retroui/Button';
 
 export function TubeActions({
-  name,
+  serverId,
+  tube,
   pause,
   pauseTimeLeft,
   onMutate,
 }: {
-  name: string;
+  serverId: number;
+  tube: string;
   pause: number;
   pauseTimeLeft: number;
   onMutate: () => void;
 }) {
   const trpc = useTRPC();
   const pauseTube = useMutation(trpc.tubes.pause.mutationOptions({ onMutate }));
-  const resumeTube = useMutation(
-    trpc.tubes.resume.mutationOptions({ onMutate }),
-  );
   const paused = pause > 0 && pauseTimeLeft > 0;
 
   return paused ? (
     <Button
-      disabled={resumeTube.isPending}
+      disabled={pauseTube.isPending}
       size="icon"
-      onClick={() => resumeTube.mutate(name)}
+      onClick={() => pauseTube.mutate({ serverId, tube, seconds: 0 })}
     >
-      <Play className="w-4 h-4" />
+      {pauseTube.isPending ? (
+        <Hourglass className="w-4 h-4" />
+      ) : (
+        <Play className="w-4 h-4" />
+      )}
     </Button>
   ) : (
     <Button
       disabled={pauseTube.isPending}
       size="icon"
-      onClick={() => pauseTube.mutate([name, 99999])}
+      onClick={() => pauseTube.mutate({ serverId, tube, seconds: 999999 })}
     >
-      <Pause className="w-4 h-4" />
+      {pauseTube.isPending ? (
+        <Hourglass className="w-4 h-4" />
+      ) : (
+        <Pause className="w-4 h-4" />
+      )}
     </Button>
   );
 }
