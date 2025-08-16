7a26b90eac81e871f3c64907d827b8b1f5b96365
Fatih Kaya <1994274@gmail.com>
2025-08-16 12:03:08 +0300

    resume/pause tubes

--- server/router.ts
+++ server/router.ts
@@ -1,10 +1,39 @@
-import { publicProcedure, router } from './trpc';
+import type { TubeStats } from 'beanstalkd-ts';
 import { bsClient } from './beanstalkd';
+import { publicProcedure, router } from './trpc';
+import z from 'zod';
+
+export interface TubeWithStats {
+  name: string;
+  stats: TubeStats;
+}
 
 export const appRouter = router({
   tubes: {
+    pause: publicProcedure
+      .input(z.tuple([z.string(), z.int()]))
+      .mutation(async (opts) => {
+        await bsClient.pauseTube(opts.input[0], opts.input[1]);
+
+        return 'ok';
+      }),
+    resume: publicProcedure.input(z.string()).mutation(async (opts) => {
+      await bsClient.pauseTube(opts.input, 0);
+
+      return 'ok';
+    }),
     list: publicProcedure.query(async () => {
-      return await bsClient.listTubes();
+      const tubes = await bsClient.listTubes();
+      const tubeStats: TubeStats[] = [];
+
+      for (const tube of tubes) {
+        tubeStats.push(await bsClient.statsTube(tube));
+      }
+
+      return tubes.map(
+        (tube, i) =>
+          ({ name: tube, stats: tubeStats[i] }) satisfies TubeWithStats,
+      );
     }),
   },
 });
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,107 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { Pause, Play } from 'lucide-react';
+import type { TubeWithStats } from '../../server/router';
 import { DataTable } from '../components/datatable';
+import { Button } from '../components/retroui/Button';
 import { useTRPC } from '../utils/trpc';
 
 export default function HomePage() {
   const trpc = useTRPC();
   const result = useQuery(trpc.tubes.list.queryOptions());
-  const table = useReactTable<string>({
+  const pauseTube = useMutation(
+    trpc.tubes.pause.mutationOptions({ onMutate: () => result.refetch() }),
+  );
+  const resumeTube = useMutation(
+    trpc.tubes.resume.mutationOptions({ onMutate: () => result.refetch() }),
+  );
+  const table = useReactTable<TubeWithStats>({
     columns: [
-      { id: 'name', header: 'tube name', cell: (ctx) => ctx.row.original },
+      { id: 'name', header: 'tube name', cell: (ctx) => ctx.row.original.name },
+      {
+        id: 'cmdDelete',
+        cell: ({ row }) => row.original.stats.cmdDelete,
+        header: 'Deletes',
+      },
+      {
+        id: 'cmdPauseTube',
+        cell: ({ row }) => row.original.stats.cmdPauseTube,
+        header: 'Pauses',
+      },
+      {
+        id: 'currentJobsBuried',
+        cell: ({ row }) => row.original.stats.currentJobsBuried,
+        header: 'Buried Jobs',
+      },
+      {
+        id: 'currentJobsDelayed',
+        cell: ({ row }) => row.original.stats.currentJobsDelayed,
+        header: 'Delayed Jobs',
+      },
+      {
+        id: 'currentJobsReady',
+        cell: ({ row }) => row.original.stats.currentJobsReady,
+        header: 'Ready Jobs',
+      },
+      {
+        id: 'currentJobsReserved',
+        cell: ({ row }) => row.original.stats.currentJobsReserved,
+        header: 'Reserved Jobs',
+      },
+      {
+        id: 'currentJobsUrgent',
+        cell: ({ row }) => row.original.stats.currentJobsUrgent,
+        header: 'Urgent Jobs',
+      },
+      {
+        id: 'currentUsing',
+        cell: ({ row }) => row.original.stats.currentUsing,
+        header: 'Producers',
+      },
+      {
+        id: 'currentWaiting',
+        cell: ({ row }) => row.original.stats.currentWaiting,
+        header: 'Consumers Waiting',
+      },
+      {
+        id: 'currentWatching',
+        cell: ({ row }) => row.original.stats.currentWatching,
+        header: 'Consumers',
+      },
+      {
+        id: 'totalJobs',
+        cell: ({ row }) => row.original.stats.totalJobs,
+        header: 'Total Jobs',
+      },
+      {
+        id: 'actions',
+        header: 'Actions',
+        cell: ({
+          row: {
+            original: { name, stats },
+          },
+        }) => {
+          const paused = stats.pause > 0 && stats.pauseTimeLeft > 0;
+
+          return paused ? (
+            <Button
+              disabled={resumeTube.isPending}
+              size="icon"
+              onClick={() => resumeTube.mutate(name)}
+            >
+              <Play className="w-4 h-4" />
+            </Button>
+          ) : (
+            <Button
+              disabled={pauseTube.isPending}
+              size="icon"
+              onClick={() => pauseTube.mutate([name, 99999])}
+            >
+              <Pause className="w-4 h-4" />
+            </Button>
+          );
+        },
+      },
     ],
     data: result.data ?? [],
     getCoreRowModel: getCoreRowModel(),
